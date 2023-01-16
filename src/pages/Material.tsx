b48939fc4290514b49629859f7f140c550542797
Justin <jgjudd40@gmail.com>
2023-01-16 17:24:23 -0500

    pull links out of each page cuz they are in the header now

--- src/pages/Material.tsx
+++ src/pages/Material.tsx
@@ -1,13 +1,7 @@
-import { Link } from 'react-router-dom'
 
 const Material = (): JSX.Element => {
   return (
     <div>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/styled'>Styled Components</Link>
-        <Link to='/tailwind'>Tailwind</Link>
-      </div>
       Material
     </div>
   )
--- src/pages/Tailwind.tsx
+++ src/pages/Tailwind.tsx
@@ -1,13 +1,7 @@
-import { Link } from 'react-router-dom'
 
 const Tailwind = (): JSX.Element => {
   return (
     <div>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/styled'>Styled Components</Link>
-        <Link to='/material'>Material UI</Link>
-      </div>
       Tailwind
     </div>
   )
